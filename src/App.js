e185da839688e106e8bccf31b4bb6bdde6c0470b
xiaodeng <927561237@qq.com>
2021-08-03 23:58:35 +0800

    引入路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+// 引入路由
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 import './App.less'
 // import 'antd/dist/antd.less' 按需打包了 就不用写这个了
 
@@ -7,7 +10,13 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Button type="primary">Primary Button</Button>
+        <BrowserRouter>
+          {/* Switch 只匹配其中一个 */}
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/" component={Layout} />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
