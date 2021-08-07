a8ec7d2383873b07d2c1f4d2406aa8b85ef4513a
xiaodeng <927561237@qq.com>
2021-08-07 15:21:36 +0800

    搭建商品整体路由

---
+++ src/pages/Product/ProductHome/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class ProductHome extends Component {
+  render() {
+    return <div>ProductHome</div>
+  }
+}
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import ProductAddUpdate from './ProductAddUpdate'
+import ProductDetail from './ProductDetail'
+import ProductHome from './ProductHome'
 
 export default class Product extends Component {
   render() {
-    return <div>产品</div>
+    return (
+      <div>
+        <Switch>
+          {/* exact  列如 精确匹配下的 ‘/’ 加后缀是匹配不到的，只能有访问 ’/‘才能跳转 */}
+          <Route exact path="/product" component={ProductHome} />
+          <Route path="/product/addupdate" component={ProductAddUpdate} />
+          <Route path="/product/detail" component={ProductDetail} />
+          <Redirect to="/product" />
+        </Switch>
+      </div>
+    )
   }
 }
